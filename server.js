a74f4a1db28955571cec97e0595868369ea4a8db
Christopher Martinez <crmartinez239@protonmail.com>
2025-11-27 20:55:19 -0500

    Change code end point to codes and accept batch request

--- server.js
+++ server.js
@@ -5,28 +5,30 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/code/:areaCode", (req, res) => {
-  const areaCode = Number(req.params.areaCode);
+app.get("/codes/:areaCodes", (req, res) => {
+  const codes = req.params.areaCodes.split(",");
+  
+  const reports = {};
+  
+  for (const code of codes) {
+  
+    // Validate input
+    if (isNaN(code) || code < 200 || code > 999) {
+      reports[code] = { ERROR: "Invalid area code" };
+      continue;
+    }
+    
+    const report = npaReport[code];
 
-  // Validate input
-  if (isNaN(areaCode)) {
-    return res.status(400).json({ error: "areaCode must be a number." });
+    if (!report) {
+      reports[code] = { ERROR: "Not in use" };
+      continue;
+    }
+    
+    reports[code] = report;
   }
 
-  if (areaCode < 200 || areaCode > 999) {
-    return res
-      .status(400)
-      .json({ error: "areaCode must be between 200 and 999." });
-  }
-
-  // Lookup timezone
-  const report = npaReport[areaCode];
-
-  if (!report) {
-    return res.status(404).json({ error: "Not in use." });
-  }
-
-  return res.json(report);
+    return res.json(reports);
 });
 
 app.listen(3000, () => console.log("API running on port 3000"));
